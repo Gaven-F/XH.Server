60673a0604e434fd18ab0f4a042c60dd06f9986a
gaven <gavenf2023@outlook.com>
2023-11-10 11:59:13 +0800

    添加钉钉开发包
    为啥有两个版本啊？

---
+++ XH.Core/DT/DTContext.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using AlibabaCloud.SDK.Dingtalkoauth2_1_0;
+using AlibabaCloud.SDK.Dingtalkoauth2_1_0.Models;
+using DingTalk.Api;
+using DingTalk.Api.Request;
+using DingTalk.Api.Response;
+using Furion.Logging;
+using Tea;
+
+namespace XH.Core.DT;
+public class DTContext
+{
+    private static string _APP_KEY = "dingifzl2bbmtmajc48t";
+    private static string _APP_SECRET = "7r8UJvYuLl81pmksnak--ssRvzesG9H4nc7PydTOfOMxShFDS1w1R7qvZFOrUYoz";
+
+    /**
+       * 使用 Token 初始化账号Client
+       * @return Client
+       * @throws Exception
+       */
+    private static Client CreateClient()
+    {
+        var config = new AlibabaCloud.OpenApiClient.Models.Config
+        {
+            Protocol = "https",
+            RegionId = "central",
+        };
+        return new Client(config);
+    }
+
+    private static string GetAccessToken()
+    {
+        // TODO:
+        // Token的缓存时间为2h，尝试进行TOKEN有效时间判断，防止每次在有效时间内进行重复获取Token
+
+        var token = "ERROR";
+        var client = CreateClient();
+        var getAccessToken = new GetAccessTokenRequest()
+        {
+            AppKey = _APP_KEY,
+            AppSecret = _APP_SECRET
+        };
+
+        try
+        {
+            token = client.GetAccessToken(getAccessToken).Body.AccessToken;
+        }
+        catch (TeaException err)
+        {
+            Log.Warning(err.Message);
+        }
+        catch (Exception _err)
+        {
+            var err = new TeaException(new Dictionary<string, object>
+            {{"message",_err.Message}});
+            if (!AlibabaCloud.TeaUtil.Common.Empty(err.Code) && !AlibabaCloud.TeaUtil.Common.Empty(err.Message))
+            {
+                Log.Warning(err.Message);
+            }
+        }
+
+        return token;
+    }
+
+    private static string GetUserInfo()
+    {
+        var client = new DefaultDingTalkClient("https://oapi.dingtalk.com/topapi/v2/user/getuserinfo");
+        var req = new OapiV2UserGetuserinfoRequest();
+        var res = client.Execute(req, GetAccessToken());
+
+        return res.Body;
+    }
+}
---
+++ XH.Demo/Program.cs
@@ -0,0 +1,68 @@
+﻿// This file is auto-generated, don't edit it. Thanks.
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+
+using Tea;
+
+
+
+
+namespace AlibabaCloud.SDK.Sample
+{
+    public class Sample
+    {
+
+        /**
+         * 使用 Token 初始化账号Client
+         * @return Client
+         * @throws Exception
+         */
+        public static AlibabaCloud.SDK.Dingtalkoauth2_1_0.Client CreateClient()
+        {
+            AlibabaCloud.OpenApiClient.Models.Config config = new AlibabaCloud.OpenApiClient.Models.Config();
+            config.Protocol = "https";
+            config.RegionId = "central";
+            return new AlibabaCloud.SDK.Dingtalkoauth2_1_0.Client(config);
+        }
+
+        public static void Main(string[] args)
+        {
+            AlibabaCloud.SDK.Dingtalkoauth2_1_0.Client client = CreateClient();
+            AlibabaCloud.SDK.Dingtalkoauth2_1_0.Models.GetAccessTokenRequest getAccessTokenRequest = new AlibabaCloud.SDK.Dingtalkoauth2_1_0.Models.GetAccessTokenRequest
+            {
+                AppKey = "dingifzl2bbmtmajc48t",
+                AppSecret = "7r8UJvYuLl81pmksnak--ssRvzesG9H4nc7PydTOfOMxShFDS1w1R7qvZFOrUYoz",
+            };
+            try
+            {
+                var res = client.GetAccessToken(getAccessTokenRequest);
+                Console.WriteLine(res.Body.AccessToken);
+            }
+            catch (TeaException err)
+            {
+                if (!AlibabaCloud.TeaUtil.Common.Empty(err.Code) && !AlibabaCloud.TeaUtil.Common.Empty(err.Message))
+                {
+                    // err 中含有 code 和 message 属性，可帮助开发定位问题
+                    Console.WriteLine(err.Message);
+                }
+            }
+            catch (Exception _err)
+            {
+                TeaException err = new TeaException(new Dictionary<string, object>
+                {
+                    { "message", _err.Message }
+                });
+                if (!AlibabaCloud.TeaUtil.Common.Empty(err.Code) && !AlibabaCloud.TeaUtil.Common.Empty(err.Message))
+                {
+                    Console.WriteLine(err.Message);
+                    // err 中含有 code 和 message 属性，可帮助开发定位问题
+                }
+            }
+        }
+
+
+    }
+}
