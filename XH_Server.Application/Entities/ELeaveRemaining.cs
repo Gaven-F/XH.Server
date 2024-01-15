a7a2d3c2cd66d9b914bca05e77b2a773aae502ae
fallen gaven <gavenf2023@outlook.com>
2024-01-15 16:19:27 +0800

    feat: :sparkles: 假期时间设置

---
+++ XH_Server.Application/Entities/ELeaveRemaining.cs
@@ -0,0 +1,9 @@
+﻿using XH_Server.Domain.Basic;
+
+namespace XH_Server.Application.Entities;
+public class ELeaveRemaining : BasicEntity
+{
+	public string UserId { get; set; } = string.Empty;
+	public string LeaveType { get; set; } = string.Empty;
+	public int Cnt { get; set; }
+}
---
+++ XH_Server.Web/Controllers/Entity/LeaveRemaining.cs
@@ -0,0 +1,41 @@
+﻿using Furion.DynamicApiController;
+using Microsoft.AspNetCore.Mvc;
+using XH_Server.Application.Entities;
+using XH_Server.Domain.Repository;
+
+namespace XH_Server.Web.Controllers.Entity;
+/// <summary>
+/// 假期剩余
+/// </summary>
+public class LeaveRemaining(IRepositoryService<ELeaveRemaining> repository) : IDynamicApiController
+{
+
+	public int GetLeaveDate(string userId, string leaveType)
+	{
+		return repository.GetData(false)
+			.FirstOrDefault(it => it.UserId == userId && it.LeaveType == leaveType)?.Cnt ?? 0;
+	}
+
+	[HttpPost]
+	public void SetLeaveDate(ELeaveRemaining data)
+	{
+		repository.SaveData(data);
+	}
+
+	[HttpPost]
+	public void SetLeaveDates(IEnumerable<ELeaveRemaining> data)
+	{
+		foreach (var item in data)
+		{
+			repository.SaveData(item);
+		}
+	}
+
+	[HttpPut]
+	public void Update(string id, int cnt)
+	{
+		var e = repository.GetDataById(Convert.ToInt64(id));
+		e.Cnt = cnt;
+		repository.UpdateData(e);
+	}
+}
