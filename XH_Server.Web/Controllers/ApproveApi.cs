2a0dc968deff12bda371462f32573d0b8dc05583
GavenF <GavenF2023@outlook.com>
2024-03-15 13:43:15 +0800

    feat: :sparkles: 可删除的审核策略

--- XH_Server.Web/Controllers/ApproveApi.cs
+++ XH_Server.Web/Controllers/ApproveApi.cs
@@ -1,12 +1,13 @@
 ﻿using Mapster;
 using Microsoft.AspNetCore.Mvc;
 using XH_Server.Application.Entities.Dto;
+using XH_Server.Core.Database;
 using XH_Server.Domain.ApprocedPolicy;
 
 namespace XH_Server.Web.Controllers;
 
 [ApiDescriptionSettings(Order = 99)]
-public class ApproveApi(ApprovedPolicyService approveService) : ControllerBase
+public class ApproveApi(ApprovedPolicyService approveService, DatabaseService dbService) : ControllerBase
 {
 	public void CreateApproval(EApprovedPolicy data)
 	{
@@ -24,5 +25,18 @@ public class ApproveApi(ApprovedPolicyService approveService) : ControllerBase
 	{
 		var p = approveService.GetLogs(Convert.ToInt64(entityId));
 		return p.Adapt<List<Vo.ApproLog>>();
+	}
+
+	/// <summary>
+	/// 删除策略
+	/// </summary>
+	/// <param name="policyId"></param>
+	/// <returns></returns>
+	public int DeleteApproval(string policyId) {
+		var id = Convert.ToInt64(policyId);
+        return dbService.Instance.Updateable<EApprovedPolicy>()
+            .Where(it => it.Id == id)
+            .SetColumns(it => new EApprovedPolicy() { IsDeleted = true, UpdateTime = DateTime.Now })
+            .ExecuteCommand();
 	}
 }
