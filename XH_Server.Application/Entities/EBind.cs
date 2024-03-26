8568cf88b5c66f61d5e41032cce161e2e016ab1a
fallen gaven <gavenf2023@outlook.com>
2024-03-26 10:18:23 +0800

    feat: 添加设备绑定数据

---
+++ XH_Server.Application/Entities/EBind.cs
@@ -0,0 +1,9 @@
+﻿using XH_Server.Domain.Basic;
+
+namespace XH_Server.Application.Entities;
+
+public class EBind : BasicEntity
+{
+	public string EquipmentId { get; set; } = string.Empty;
+	public string UserId { get; set; } = string.Empty;
+}
