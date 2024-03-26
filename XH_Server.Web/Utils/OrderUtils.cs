2e93b615d9d268e2ee259519a47312f0e64392e5
fallen gaven <gavenf2023@outlook.com>
2024-03-26 18:31:27 +0800

    feat: 订单导出完成

--- XH_Server.Web/Utils/OrderUtils.cs
+++ XH_Server.Web/Utils/OrderUtils.cs
@@ -1,79 +1,135 @@
 ﻿using System.Text.RegularExpressions;
+using XH_Server.Application.Entities;
 
 namespace XH_Server.Web.Utils;
 
 public static partial class OrderUtils
 {
-    class TemplateFileInfo(string name, string path, string description = "")
-    {
-        public string Name { get; set; } = name;
-        public string? Description { get; set; } = description;
-        public string Path { get; set; } = path;
-        public string FullPath { get => System.IO.Path.Combine(Path, Name); }
-    }
+	private class TemplateFileInfo(string name, string path, string description = "")
+	{
+		public string Name { get; set; } = name;
+		public string? Description { get; set; } = description;
+		public string Path { get; set; } = path;
+		public string FullPath { get => System.IO.Path.Combine(Path, Name); }
+	}
 
-    private static readonly Regex placeHolder = R_REPLACE_HOLDER();
-    private static readonly string templateFilePath = "_template";
-    private static readonly string absTemplateFilePath = Path.Combine(Environment.CurrentDirectory.ToString(), templateFilePath);
+	private static readonly Regex placeHolder = R_REPLACE_HOLDER();
+	private static readonly Regex arrPlaceHolder = R_ARR_REPLACE_HOLDER();
+	private static readonly string templateFilePath = "_template";
+	private static readonly string absTemplateFilePath = Path.Combine(Environment.CurrentDirectory.ToString(), templateFilePath);
 
-    private static readonly List<TemplateFileInfo> templateFiles = [
-        new ("随工单.docx", templateFilePath)
-    ];
+	private static readonly List<TemplateFileInfo> templateFiles = [
+		new ("随工单.docx", templateFilePath)
+	];
 
-    private static TemplateFileInfo GetInfoByName(string name)
-    {
-        return templateFiles.FirstOrDefault(it => it.Name == name) ?? throw new("不存在的模板文件名！");
-    }
+	private static TemplateFileInfo GetInfoByName(string name)
+	{
+		return templateFiles.FirstOrDefault(it => it.Name == name) ?? throw new("不存在的模板文件名！");
+	}
 
-    static OrderUtils()
-    {
-        if (!Directory.Exists(absTemplateFilePath)) throw new("模板文件不存在！");
-    }
+	static OrderUtils()
+	{
+		if (!Directory.Exists(absTemplateFilePath)) throw new("模板文件不存在！");
+	}
 
-    private static IEnumerable<N_Run> GetDocRuns(N_Doc doc) => from P in doc.Paragraphs
-                                                               from r in P.Runs
-                                                               select r;
+	private static IEnumerable<N_Par> GetDocPars(N_Doc doc) => from P in doc.Paragraphs
+															   select P;
 
-    private static IEnumerable<N_Run> GetDocTableRuns(N_Doc doc) => from T in doc.Tables
-                                                                    from R in T.Rows
-                                                                    from C in R.GetTableCells()
-                                                                    from P in C.Paragraphs
-                                                                    from r in P.Runs
-                                                                    select r;
+	private static IEnumerable<N_Par> GetDocTablePars(N_Doc doc) => from T in doc.Tables
+																	from R in T.Rows
+																	from C in R.GetTableCells()
+																	from P in C.Paragraphs
+																	select P;
 
-    public static Stream Demo()
-    {
-        var stream = new NPOIStream(false);
-        using var fs = new FileStream(GetInfoByName("随工单.docx").FullPath, FileMode.Open);
-        using var docx = new N_Doc(fs);
+	public static Stream Demo()
+	{
+		var stream = new NPOIStream(false);
+		using var fs = new FileStream(GetInfoByName("随工单.docx").FullPath, FileMode.Open);
+		using var docx = new N_Doc(fs);
 
-        var runs = new List<N_Run>();
+		var runs = new List<N_Par>();
 
-        runs.AddRange(GetDocRuns(docx));
-        runs.AddRange(GetDocTableRuns(docx));
-        runs.ForEach(r =>
-        {
-            var text = r.Text;
-            if (placeHolder.Matches(text) is IEnumerable<Match> ms && ms.Any())
-            {
-                foreach (Match m in ms)
-                {
-                    // TODO 根据不同占位符获取不同的值
-                    r.ReplaceText(m.Groups[0].Value, "REPLACE");
-                }
-            }
-        });
+		runs.AddRange(GetDocPars(docx));
+		runs.AddRange(GetDocTablePars(docx));
+		runs.ForEach(r =>
+		{
+			var text = r.Text;
+			if (placeHolder.Matches(text) is IEnumerable<Match> ms && ms.Any())
+			{
+				foreach (Match m in ms)
+				{
 
-        docx.Write(stream);
-        stream.Flush();
-        stream.Seek(0, SeekOrigin.Begin);
-        stream.AllowClose = true;
+					// TODO 根据不同占位符获取不同的值
+					r.ReplaceText(m.Groups[0].Value, "REPLACE");
+				}
+			}
+		});
 
-        return stream;
-    }
+		docx.Write(stream);
+		stream.Flush();
+		stream.Seek(0, SeekOrigin.Begin);
+		stream.AllowClose = true;
 
-    [GeneratedRegex(@"\{\{(.*?)\}\}")]
-    private static partial Regex R_REPLACE_HOLDER();
+		return stream;
+	}
+
+	public static Stream ReplaceByEntity(EOrder e)
+	{
+		var stream = new NPOIStream(false);
+		using var fs = new FileStream(GetInfoByName("随工单.docx").FullPath, FileMode.Open);
+		using var docx = new N_Doc(fs);
+
+		var runs = new List<N_Par>();
+
+		runs.AddRange(GetDocPars(docx));
+		runs.AddRange(GetDocTablePars(docx));
+
+		var t1 = typeof(EOrder);
+		var t2 = typeof(EOrderItem);
+
+		runs.ForEach(r =>
+		{
+			var text = r.Text;
+			if (placeHolder.Matches(text) is IEnumerable<Match> ms && ms.Any())
+			{
+				foreach (Match m in ms)
+				{
+					string? value = null;
+					if (arrPlaceHolder.Match(m.Groups[0].Value) is Match am && am.Success)
+					{
+						var p = t2.GetProperty(am.Groups[1].Value);
+						if (p != null)
+						{
+							var index = Convert.ToInt32(am.Groups[2].Value);
+							if (e.Items != null && e.Items.Count > index) value = Convert.ToString(p.GetValue(e.Items[index]));
+						}
+					}
+					else
+					{
+						var p = t1.GetProperty(m.Groups[1].Value);
+						if (p != null)
+						{
+							value = Convert.ToString(p.GetValue(e));
+						}
+					}
+					r.ReplaceText(m.Groups[0].Value, value);
+				}
+			}
+		});
+
+		docx.Write(stream);
+		stream.Flush();
+		stream.Seek(0, SeekOrigin.Begin);
+		stream.AllowClose = true;
+
+		return stream;
+	}
+
+	[GeneratedRegex(@"\{\{(.*?)\}\}")]
+	private static partial Regex R_REPLACE_HOLDER();
+
+	[GeneratedRegex(@"\{\{(\w+)\[([0-9]+)\]\}\}")]
+	private static partial Regex R_ARR_REPLACE_HOLDER();
 }
 
 
