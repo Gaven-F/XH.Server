2cd9ac16bf6b07b5311b002dcd2c22765a06558e
GavenF <GavenF2023@outlook.com>
2024-04-02 15:45:20 +0800

    feat(leave): 请假接口修改

--- XH_Server.Web/Controllers/Entity/LeaveRemaining.cs
+++ XH_Server.Web/Controllers/Entity/LeaveRemaining.cs
@@ -10,32 +10,49 @@ namespace XH_Server.Web.Controllers.Entity;
 public class LeaveRemaining(IRepositoryService<ELeaveRemaining> repository) : IDynamicApiController
 {
 
-	public int GetLeaveDate(string userId, string leaveType)
-	{
-		return repository.GetData(false)
-			.FirstOrDefault(it => it.UserId == userId && it.LeaveType == leaveType)?.Cnt ?? 0;
-	}
+    public ELeaveRemaining GetLeaveDate(string userId, string leaveType)
+    {
+        return repository.GetData(false)
+            .FirstOrDefault(it => it.UserId == userId && it.LeaveType == leaveType) ?? new ELeaveRemaining();
+    }
 
-	[HttpPost]
-	public void SetLeaveDate(ELeaveRemaining data)
-	{
-		repository.SaveData(data);
-	}
+    [HttpPost]
+    public void SetLeaveDate(ELeaveRemaining data)
+    {
+        repository.SaveData(data);
+    }
 
-	[HttpPost]
-	public void SetLeaveDates(IEnumerable<ELeaveRemaining> data)
-	{
-		foreach (var item in data)
-		{
-			repository.SaveData(item);
-		}
-	}
+    [HttpPost]
+    public void SetLeaveDates(IEnumerable<ELeaveRemaining> data)
+    {
+        foreach (var item in data)
+        {
+            repository.SaveData(item);
+        }
+    }
 
-	[HttpPut]
-	public void Update(string id, int cnt)
-	{
-		var e = repository.GetDataById(Convert.ToInt64(id));
-		e.Cnt = cnt;
-		repository.UpdateData(e);
-	}
+    [HttpPut]
+    public void Update(string id, int cnt)
+    {
+        var e = repository.GetDataById(Convert.ToInt64(id));
+        e.Cnt = cnt;
+        repository.UpdateData(e);
+    }
+
+    [HttpPut]
+    public void Minus(string id, int cnt)
+    {
+        var e = repository.GetDataById(Convert.ToInt64(id));
+        e.Cnt -= cnt;
+        repository.UpdateData(e);
+    }
+
+    [HttpDelete("{id}")]
+    public int Delete(string id)
+    {
+        var i = Convert.ToInt64(id);
+        var data = repository.GetDataById(i);
+        data.IsDeleted = true;
+        return repository.UpdateData(data);
+    }
 }
